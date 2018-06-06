d7f2ea098e6361f35293c4efa21c695568b2e84d
Jonathan <jonathan.catchpowle@gmail.com>
2018-06-06 21:43:46 +0100

    Increase carousel sliding speed

--- src/slider/index.js
+++ src/slider/index.js
@@ -8,7 +8,7 @@ class SimpleSlider extends Component {
     const settings = {
       dots: true,
       infinite: true,
-      speed: 500,
+      speed: 200,
       slidesToShow: 1,
       slidesToScroll: 1
     };
