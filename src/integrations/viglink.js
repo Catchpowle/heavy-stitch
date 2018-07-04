edbfea3015b3f7a43dd7e2b34bccf1eec6c5df55
Jonathan <jonathan.catchpowle@gmail.com>
2018-07-04 23:50:11 +0100

    Add integrations

---
+++ src/integrations/viglink.js
@@ -0,0 +1,10 @@
+var vglnk = { key: process.env.REACT_APP_VIGLINK };
+
+(function(d, t) {
+  var s = d.createElement(t);
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//cdn.viglink.com/api/vglnk.js';
+  var r = d.getElementsByTagName(t)[0];
+  r.parentNode.insertBefore(s, r);
+}(document, 'script'));
